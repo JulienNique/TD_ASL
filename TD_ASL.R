3cae66bbc191daca63ba1a410ead384fc56109a1
Julien Nique <nique.julien@neuf.fr>
2020-09-26 10:43:27 +0200

    Update Exercise 1

--- TD_ASL.R
+++ TD_ASL.R
@@ -1,6 +1,8 @@
 ## Exercise 1 ####
 x <- seq(1,7,by = 1)
 y <- c(6,4,2,1,3,6,10)
+
+#méthode paramétrique
 model <- lm(y ~ x)
 coeff <- model$coefficients
 d <- function(x){
@@ -8,3 +10,7 @@ d <- function(x){
 }
 plot(x,y)
 lines(x,d(x),col="green")
+
+#méthode non paramétrique
+y2 = c(5,4,7/3,2,10/3,19/3,8)
+points(x,y2,col="red")
